8fdc766f1bfdd724305b7bc192dbfdb4b21665e8
Shivaay09 <135844761+Shivaay09@users.noreply.github.com>
2025-02-11 04:30:30 +0530

    Delete backend directory

--- backend/src/lib/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-export const connectDB = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGODB_URI)
-        console.log(`MongoDB connected: ${connection.connection.host}`)
-    }catch(err) {
-        console.error(`MongoDB connection error: ${err}`)
-    }
-}
